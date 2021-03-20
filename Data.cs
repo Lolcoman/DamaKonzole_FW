ee2bbdc8f1f0d1b057b4183145da2ec5daa3a201
Lolcoman <michaelkozak.cz@gmail.com>
2021-03-20 16:33:58 +0100

    load/save

--- Data.cs
+++ Data.cs
@@ -9,6 +9,13 @@ namespace DamaKonzole_Framework
 {
     class Data
     {
+        /// <summary>
+        /// Metoda pro uložení hry
+        /// </summary>
+        /// <param name="player1"></param>
+        /// <param name="player2"></param>
+        /// <param name="ptrTah"></param>
+        /// <param name="historie"></param>
         public void SaveGame(int player1, int player2, int ptrTah, List<int[]> historie)
         {
             using (StreamWriter sw = new StreamWriter(@"test.txt"))
@@ -29,6 +36,49 @@ namespace DamaKonzole_Framework
                 sw.Flush();
             }
         }
+        public void LoadGame()
+        {
+            using (StreamReader sr = new StreamReader(@"test.txt"))
+            {
+                string prvniRadek = sr.ReadLine();
+                char hrac1 = prvniRadek[8];
+                int bily = (int)(hrac1 - '0');
+
+                string druhyRadek = sr.ReadLine();
+                char hrac2 = druhyRadek[8];
+                int cerny = (int)(hrac2 - '0');
+
+                string tretiRadek = sr.ReadLine();
+                char ptr = tretiRadek[8];
+                int ukazatel = (int)(ptr - '0');
+
+                List<int[]> seznam = new List<int[]>();
+                string historieTahu;
+                while ((historieTahu = sr.ReadLine()) != null)
+                {
+                    int[] celyPohyb = new int[0];
+                    for (int i = 0; i < historieTahu.Length; i += 4)
+                    {
+                        int[] castPohybu = new int[] { (historieTahu[i] - 'a'), (historieTahu[i + 1] - '1'), CharToStone(historieTahu[i + 2]), CharToStone(historieTahu[i + 3]) };
+                        celyPohyb = celyPohyb.Concat(castPohybu).ToArray();
+                    }
+                    seznam.Add(celyPohyb);
+                }
+                //foreach (int[] item in seznam)
+                //{
+                //    for (int i = 0; i < item.Length; i = i + 4)
+                //    {
+                //        Console.Write("{0}{1}{2}{3} ", (char)(item[i] + 'a'), (char)(item[1 + i] + '1'), StoneToString(item[2 + i]), StoneToString(item[3 + i]));
+                //    }
+                //    Console.WriteLine();
+                //}
+            }
+        }
+        /// <summary>
+        /// Metoda pro převod kamenů na string
+        /// </summary>
+        /// <param name="stone"></param>
+        /// <returns></returns>
         public string StoneToString(int stone)
         {
             switch (stone)
@@ -45,18 +95,23 @@ namespace DamaKonzole_Framework
                     return "0";
             }
         }
+        /// <summary>
+        /// Metoda pro převod znaků na kameny
+        /// </summary>
+        /// <param name="stone"></param>
+        /// <returns></returns>
         public int CharToStone(char stone)
         {
             switch (stone)
             {
                 case 'w':
-                    return 0;
+                    return 1;
                 case 'W':
-                    return 0;
+                    return 2;
                 case 'b':
                     return -1;
                 case 'B':
-                    return -1;
+                    return -2;
                 default:
                     return 0;
             }
